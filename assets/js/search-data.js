a05ecdeddf898157a1ceec8b450c6ea66a7aebf1
an-minyoung <119140502+an-minyoung@users.noreply.github.com>
2026-07-31 06:52:21 +0000

    Deploying to gh-pages from @ an-minyoung/an-minyoung.github.io@67a20b38a4c6cdd91952cdc27d73939d85f5a116 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -152,7 +152,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/CV.pdf", "_blank");
+          window.open("/assets/pdf/CV.pdf?v=2026-04-09", "_blank");
         },
       },{
         id: 'social-email',
